449181b2d6e4e03282e2a53b1ca1bb8e08fde757
ismailsenior110 <123931767+ismailsenior110@users.noreply.github.com>
2023-01-30 10:00:52 +0100

    Files addded

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// ********************************************************* 
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
+// Year: Trimester 1, 2022/23 (T2215) 
+// Lab: Lab12
+// Names: MUHAMMAD ISMAIL HAMZA 
+// IDs: 1201303165
+// Emails: 1201303165@student.mmu.edu.my
+// Phones: +60 11-26196605
+// ********************************************************* 
+
+#include "Menu.cpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Menu menu;
+    menu.welcome_msg();
+}
